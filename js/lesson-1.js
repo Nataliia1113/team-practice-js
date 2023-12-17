b3542e0d22f21757102bc5be09f9095861a46d2c
DaryPet <d7akkord@gmail.com>
2023-12-17 16:01:01 +0100

    task-4

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -58,3 +58,28 @@
 // }
 
 // console.log(message(35));
+
+// task-4
+
+//TODO:===================================
+//~ Змінна num може набувати 4 значення: 1, 2, 3 або 4. Якщо вона має
+//~ значення '1', то у змінну result запишемо 'зима', якщо має значення
+//~ '2' - 'весна' і так далі. Розв'яжіть завдання через switch-case.
+// let valueSeason = prompt("Введіть число від 1 до 4");
+// console.log(valueSeason);
+// switch (Number(valueSeason)) {
+//     case 1:
+//         console.log("зима");
+//         break;
+//     case 2:
+//         console.log("весна");
+//         break;
+//     case 3:
+//         console.log("summer");
+//         break;
+//     case 4:
+//         console.log("autumn");
+//         break;
+//     default:
+//         console.log("invalid");
+// }    
