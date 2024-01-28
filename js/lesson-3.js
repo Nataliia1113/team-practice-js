5b5a5328bef2cea1c8cb599705090bd5e68b8ec1
HalynaLysun <125215419+HalynaLysun@users.noreply.github.com>
2024-01-28 15:36:50 +0200

    task-1,2

---
+++ js/lesson-3.js
@@ -0,0 +1,39 @@
+
+/*
+~Завдання 1
+Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
+*/
+
+// const buttonEl = document.querySelector("#alertButton")
+// const inputEl = document.querySelector("#alertInput")
+// console.log(inputEl)
+
+// function pressHandler(event) {
+//     console.log(inputEl.value)
+//     console.log(event)
+// }
+
+// buttonEl.addEventListener("click", pressHandler)
+
+
+// Завдання 2
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+//
+
+// const buttonEl = document.querySelector("#swapButton")
+// const inputEl = document.querySelector("#leftSwapInput")
+// const inputElRight = document.querySelector("#rightSwapInput")
+
+
+// function pressHandler(event) {
+//     const valueInputLeft = inputEl.value
+//     const valueInputRight = inputElRight.value
+//     inputEl.value = valueInputRight
+//     inputElRight.value = valueInputLeft
+//     console.log(event)
+// }
+
+// buttonEl.addEventListener("click", pressHandler)
+
+
